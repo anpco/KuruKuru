54c819f55fd64384ab753155f370998c4ffbb460
Nhat Nguyen <iamducnhat@gmail.com>
2023-06-12 15:06:26 +0700

    Initial Commit

---
+++ KuruKuru/KuruKuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuruKuruApp.swift
+//  KuruKuru
+//
+//  Created by Nguyen Duc Nhat on 12/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct KuruKuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
